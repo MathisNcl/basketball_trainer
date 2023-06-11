a8615f59beff8fdff713c7f061a62562d3efb7ac
MathisNcl <mathis.nicoli@gmail.com>
2023-06-11 13:57:58 +0200

    🐛 circular import due to isort

--- src/bball_trainer/models/__init__.py
+++ src/bball_trainer/models/__init__.py
@@ -1,3 +1,3 @@
-from .base import Base  # noqa
-from .game_record import GameRecord  # noqa
-from .user import User  # noqa
+from .base import Base  # noqa #isort: skip
+from .user import User  # noqa #isort: skip
+from .game_record import GameRecord  # noqa #isort: skip
